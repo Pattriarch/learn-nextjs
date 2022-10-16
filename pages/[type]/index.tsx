f2b8efd8df6508e89d63b05caf7bc9b94e408fd2
Yano <89477338+Pattriarch@users.noreply.github.com>
2022-10-17 00:30:26 +0300

    14 chapter start

--- pages/[type]/index.tsx
+++ pages/[type]/index.tsx
@@ -1,12 +1,57 @@
 import { withLayout } from "../../layout/Layout";
+import {GetStaticPaths, GetStaticProps, GetStaticPropsContext} from "next";
+import {firstLevelMenu} from "../../helpers/helpers";
+import axios from "axios";
+import {MenuItem} from "../../interfaces/menu.interface";
+import {ParsedUrlQuery} from "querystring";
+import {TopLevelCategory, TopPageModel} from "../../interfaces/page.interface";
+import {ProductModel} from "../../interfaces/product.interface";
 
-function Courses(): JSX.Element {
+function Type({ firstCategory }: TypeProps): JSX.Element {
     return (
         <>
-            Courses
+            Type: {firstCategory}
         </>
     );
 }
 
-export default withLayout(Courses);
+export default withLayout(Type);
 
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        paths: firstLevelMenu.map(m => '/' + m.route),
+        fallback: true
+    };
+};
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+export const getStaticProps: GetStaticProps<TypeProps> = async ({ params }: GetStaticPropsContext<ParsedUrlQuery>) => {
+    if (!params) {
+        return {
+            notFound: true
+        };
+    }
+    const firstCategoryItem = firstLevelMenu.find(m => m.route == params.type);
+    if (!firstCategoryItem) {
+        return {
+            notFound: true
+        };
+    }
+    const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+        firstCategory: firstCategoryItem.id
+    });
+    return {
+        props: {
+            menu,
+            firstCategory: firstCategoryItem.id
+        }
+    };
+};
+
+interface TypeProps extends Record<string, unknown> {
+    menu: MenuItem[];
+    firstCategory: TopLevelCategory;
+    page: TopPageModel;
+    products: ProductModel[];
+}
--- pages/search.tsx
+++ pages/search.tsx
@@ -1,4 +1,7 @@
 import {withLayout} from "../layout/Layout";
+import {GetStaticProps} from "next";
+import axios from "axios";
+import {MenuItem} from "../interfaces/menu.interface";
 
 function Search(): JSX.Element {
     return (
@@ -10,3 +13,20 @@ function Search(): JSX.Element {
 
 export default withLayout(Search);
 
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+    const firstCategory = 0;
+    const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+        firstCategory
+    });
+    return {
+        props: {
+            menu,
+            firstCategory
+        }
+    };
+};
+
+interface HomeProps extends Record<string, unknown> {
+    menu: MenuItem[];
+    firstCategory: number;
+}
